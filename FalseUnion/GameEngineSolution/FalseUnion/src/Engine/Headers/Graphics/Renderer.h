fa14d31cb2382a911bb9413b9557f2db130b1e4b
Sky <34356003+SkyTheProgrammer@users.noreply.github.com>
2025-10-14 13:51:48 -0600

    Expanded on renderer and added comments.

--- FalseUnion/GameEngineSolution/FalseUnion/src/Engine/Headers/Graphics/Renderer.h
+++ FalseUnion/GameEngineSolution/FalseUnion/src/Engine/Headers/Graphics/Renderer.h
@@ -1,28 +1,30 @@
 //
 // Created by SkyTFB on 10/6/2025.
 //
+#pragma once
 
-#ifndef FALSEUNION_RENDERER_H
-#define FALSEUNION_RENDERER_H
 #include "Camera.h"
 #include "Shader.h"
 #include "Sprite.h"
 #include "../Math/Vector2.h"
 
-class Renderer {
+namespace FalseUnion{class Renderer {
     public:
-    Renderer();
-    void Initialize();
-    void DrawSprite(Sprite sprite);
-    void Flush();
-    void SetCamera(Camera camera);
+    Renderer(); // default constructor for renderer
+    virtual ~Renderer(); // virtual deconstructor in case of inheritance
+    static void Initialize(); // initializes the renderer
+    void DrawSprite(Sprite sprite); // draws the given sprite
+    void Flush(); // flushes unused or un-needed objects.
+    void SetCamera(Camera camera); // sets the camera to the given camera object.
 private:
-    int VAO, VBO, EBO;
-    Shader *shader;
-    std::list<Vector2> vertices;
-    std::list<int> indices;
-    Camera *camera;
-    void buffer();
-    std::string LoadShader(std::string name);
-};
-#endif //FALSEUNION_RENDERER_H
+    //
+    int VAO, VBO, EBO; // change this later for now store as int (Vertex Buffer Object VBO, Vertex Array Object VAO, and Element Buffer Object EBO)
+    //
+    Shader *shader; // assigns the shader
+    std::list<Vector2> vertices; // list of vertices
+    std::list<int> indices; // list of indices
+    Camera *camera; // reference camera
+    void buffer(); // buffer method
+    std::string LoadShader(const std::string& name); // loader for the shader based on name.
+    std::string LoadShader(int id); // loader for the shader based on name.
+};}
--- FalseUnion/GameEngineSolution/FalseUnion/src/Engine/cpp/Graphics/Renderer.cpp
+++ FalseUnion/GameEngineSolution/FalseUnion/src/Engine/cpp/Graphics/Renderer.cpp
@@ -4,24 +4,79 @@
 
 #include "../../Headers/Graphics/Renderer.h"
 
-Renderer::Renderer() {
-}
+namespace FalseUnion
+{
+    /// <summary>
+    /// Default renderer, sets everything to null/0
+    /// </summary>
+    Renderer::Renderer()
+    {
+        VAO = 0;
+        VBO = 0;
+        EBO = 0;
+        shader = nullptr;
+        camera = nullptr;
+    }
 
-void Renderer::Initialize() {
-}
+    /// <summary>
+    /// Default deconstructor in case of inheratence.
+    /// </summary>
+    Renderer::~Renderer()
+    = default;
 
-void Renderer::DrawSprite(Sprite sprite) {
-}
+    /// <summary>
+    /// Initializes the renderer
+    /// </summary>
+    void Renderer::Initialize()
+    {
+    }
 
-void Renderer::Flush() {
-}
+    /// <summary>
+    /// Draws the given sprite.
+    /// </summary>
+    /// @param sprite Sprite, the sprite to be drawn.
+    void Renderer::DrawSprite(Sprite sprite)
+    {
+    }
 
-void Renderer::SetCamera(Camera camera) {
-}
+    /// <summary>
+    /// flushes all unused/unwanted objects.
+    /// </summary>
+    void Renderer::Flush()
+    {
+    }
 
-void Renderer::buffer() {
-}
+    /// <summary>
+    /// Sets the renderer's camera to the given camera
+    /// </summary>
+    /// @param camera Camera, the camera to be set in renderer.
+    void Renderer::SetCamera(Camera camera)
+    {
+        this->camera = &camera;
+    }
 
-std::string Renderer::LoadShader(std::string name) {
-    return "";
+    /// <summary>
+    /// sets up the VAO, VBO, and EBO 
+    /// </summary>
+    void Renderer::buffer()
+    {
+    }
+
+    /// <summary>
+    /// Loads the shader by given name.
+    /// </summary>
+    /// @param name string, name of shader to be loaded
+    std::string Renderer::LoadShader(const std::string& name)
+    {
+        return "";
+    }
+
+    /// <summary>
+    /// Loads the shader by given ID.
+    /// </summary>
+    /// @param id int, id of shader to be loaded
+    std::string Renderer::LoadShader(int id)
+    {
+        return "";
+    }
 }
