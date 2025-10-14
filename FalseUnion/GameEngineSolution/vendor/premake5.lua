74e7ef42322259d7d4f9e22a274cf37ff74721a1
Sky <34356003+SkyTheProgrammer@users.noreply.github.com>
2025-10-14 13:16:14 -0600

    Removed vendor from gitignore made bin and bin-int explicit

---
+++ FalseUnion/GameEngineSolution/vendor/premake5.lua
@@ -0,0 +1,107 @@
+﻿workspace "FalseUnion"
+    architecture "x64"
+
+    configurations {
+    "Debug",
+    "Release",
+    "Dist"
+}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    
+project "FalseUnion"
+    location "FalseUnion"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    
+    filter "system:windows"
+        cppdialect "c++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FU_PLATFORM_WINDOWS",
+            "FU_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    filter "configurations:Debug"
+        defines "FU_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "FU_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "FU_DIST"
+        optimize "On"
+
+    filter {"system:windows", "configurations:Release"}
+        buildoptions "/MT"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "FalseUnion/src"
+    }
+
+    links
+    {
+        "FalseUnion"
+    }
+    
+    filter "system:windows"
+        cppdialect "c++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FU_PLATFORM_WINDOWS"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    filter "configurations:Debug"
+        defines "FU_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "FU_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "FU_DIST"
+        optimize "On"
+
+    filter {"system:windows", "configurations:Release"}
+        buildoptions "/MT"
