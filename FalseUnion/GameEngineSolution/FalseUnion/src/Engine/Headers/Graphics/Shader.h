72482e9affc3119bbf0b5323afcec8c0f943e9a0
Sky <34356003+SkyTheProgrammer@users.noreply.github.com>
2025-10-14 13:33:53 -0600

    Expanded on Shader classes and commented them

--- FalseUnion/GameEngineSolution/FalseUnion/src/Engine/Headers/Graphics/Shader.h
+++ FalseUnion/GameEngineSolution/FalseUnion/src/Engine/Headers/Graphics/Shader.h
@@ -1,21 +1,27 @@
 //
 // Created by SkyTFB on 10/6/2025.
 //
-
-#ifndef FALSEUNION_SHADER_H
-#define FALSEUNION_SHADER_H
+#pragma once
 #include <list>
 #include <string>
-class Shader {
-public:
-    Shader(int id);
-    void Bind();
-    void Unbind();
-    void SetUniform(const char *name, int value);
-private:
-    int m_ShaderID;
-    std::list<std::pair<int, std::string>> m_UniformLocationList;
-    int CompileShader(int type, const std::string *source);
-    int GetUniformLocation(const std::string &name);
-};
-#endif //FALSEUNION_SHADER_H
+
+namespace FalseUnion
+{
+    class Shader {
+    public:
+        Shader(); // default constructor for shader
+        virtual ~Shader(); // default virtual destructor in case of inheratence
+        Shader(int id); // shader constructor with id
+        void Bind(); // binds a shader 
+        void Unbind(); // unbinds a shader
+        void SetUniform(const std::string *name); //sets a shaders uniform given a name
+        void SetUniform(const std::string *name, int value); // sets a shaders uniform with a reference name and id
+        int GetUniformLocation(const std::string &name); // gets a shaders uniform location given its name.
+        int GetUniformLocation(const int id); // gets a shaders uniform location given its id.
+    private:
+        int m_ShaderID; // shaders id
+        static std::list<std::pair<int, std::string>> m_UniformLocationList; // Shaders uniform location list
+        int CompileShader(int type, const std::string *source); // compiles a shader based on name
+        int CompileShader(int type, const int id); // compiles a shader based on id
+    };
+}
--- FalseUnion/GameEngineSolution/FalseUnion/src/Engine/cpp/Graphics/Shader.cpp
+++ FalseUnion/GameEngineSolution/FalseUnion/src/Engine/cpp/Graphics/Shader.cpp
@@ -4,23 +4,97 @@
 
 #include "../../Headers/Graphics/Shader.h"
 
-Shader::Shader(int id) {
-    m_ShaderID = id;
-}
+namespace FalseUnion
+{
+    /// <summary>
+    /// Default shader constructor, assigns its id to be 0
+    /// </summary>
+    Shader::Shader()
+    {
+        m_ShaderID = 0;
+    }
 
-void Shader::Bind() {
-}
+    /// <summary>
+    /// Default deconstructor for use in case of inheritance.
+    /// </summary>
+    Shader::~Shader() = default;
 
-void Shader::Unbind() {
-}
 
-void Shader::SetUniform(const char *name, int value) {
-}
+    /// <summary>
+    /// Shader constructor that intakes and sets a shaders id.
+    /// </summary>
+    /// @param id int, to be set to shaders id
+    Shader::Shader(int id)
+    {
+        m_ShaderID = id;
+    }
 
-int Shader::GetUniformLocation(const std::string &name) {
-    return 0;
-}
+    /// <summary>
+    /// Binds a shader to object.
+    /// </summary>
+    void Shader::Bind()
+    {
+    }
 
-int Shader::CompileShader(int type, const std::string *source) {
-    return 0;
+    /// <summary>
+    /// unbinds a shader from an object.
+    /// </summary>
+    void Shader::Unbind()
+    {
+    }
+
+    /// <summary>
+    /// Sets a shaders uniform given a name.
+    /// </summary>
+    void Shader::SetUniform(const std::string* name)
+    {
+    }
+
+    /// <summary>
+    /// Sets a shaders uniform location given a name and id.
+    /// </summary>
+    /// @param name string, to be set to shaders name in uniform
+    /// @param value int, to be set to shaders id in uniform
+    void Shader::SetUniform(const std::string* name, int value)
+    {
+        
+    }
+
+    /// <summary>
+    /// returns the int value for a shaders location in the uniform list based on its name.
+    /// </summary>
+    /// @param name string, name to search for in uniform list.
+    int Shader::GetUniformLocation(const std::string& name)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// returns the int value for a shaders location in the uniform list based on its id.
+    /// </summary>
+    /// @param id int, id to search for in the uniform list.
+    int Shader::GetUniformLocation(const int id)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// Returns int based on if the shader compiled. takes in type of compile and name of shader.
+    /// </summary>
+    /// @param type int, representing the type of compile.
+    /// @param source string, representing the shaders name.
+    int Shader::CompileShader(int type, const std::string* source)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// Returns int based on if the shader compiled. takes in the type of compile and the id of shader.
+    /// </summary>
+    /// @param type int, representing the type of compile.
+    /// @param id int, representing the shaders name.
+    int Shader::CompileShader(int type, const int id)
+    {
+        return 0;
+    }
 }
