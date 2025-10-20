8e4bfb3567796cf010c07e2f9099545b8d7fe937
Sky <34356003+SkyTheProgrammer@users.noreply.github.com>
2025-10-20 10:05:24 -0600

    Finished outlining event and dispatcher.
    Finished outlining the event and event dispatcher class they should be good to start to implement into event systems for other system (window, application, input ect.)
    
    Moved Event dispatcher to Event.h as they are tightly coupled and should be fine together.
    
    Added classes to outline, tested EventDispatcher on basic KeyEvent class and it let me implement

--- FalseUnion/src/Engine/Headers/Events/Event.h
+++ FalseUnion/src/Engine/Headers/Events/Event.h
@@ -17,8 +17,8 @@ namespace FalseUnion
 
 #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
     virtual EventType GetEventType() const override { return GetStaticType();}\
-    virtual const std::string GetName() const {return #type}
-    
+    virtual const std::string GetName() const {return #type;}
+// macro to for GetStaticType(), GetEventType(), and GetName() for EventType such that I don't have to manually make it in classes that inherit from event.
     enum EventCategory
     {
         None = 0,
@@ -29,9 +29,12 @@ namespace FalseUnion
         MouseButton
     }; // enum for event category
 #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
-    
+    //macro for GetCategoryFlags(), makes it return category.
+
     class FALSEUNION_API Event
     {
+        friend class EventDispatcher;
+        // ^ defines Event dispatcher as a friend class of event allowing access to protected variables
         // Outline a block event system.
         // Events must be dealt with as they enter
         // Try and move to a buffer system asap
@@ -40,14 +43,14 @@ namespace FalseUnion
         virtual const std::string GetName() const = 0; // virtual declaration for name getter
         virtual int GetCategoryFlag() const = 0; // virtual declaration for category getter
         virtual std::string ToString() const { return GetName(); }
-        // virtual declarion for ToString, defines getname at top.
+        // virtual declaration for ToString, defines GetName at top.
 
         /// <summary>
         /// method to check if event is in provided event category
         /// </summary>
         /// @param category EventCategory to check against the event.
         /// @returns Bool true if the event is in category false otherwise.
-        bool IsInCategory(EventCategory category)
+        inline bool IsInCategory(EventCategory category)
         {
             if (category == EventCategory::Input)
             {
@@ -61,6 +64,49 @@ namespace FalseUnion
         }
 
     protected:
-        bool handled = false; // A variable to track whether an event has been handled
+        bool m_Handled = false; // A variable to track whether an event has been handled
     };
+
+    class EventDispatcher // Event Dispatcher class placed inside of event as they are tightly coupled.
+    {
+        template <typename T>
+        // Define the using EventFn so that it can be used in EventDispatcher
+        // T represents a class that inherits from event.
+        using EventFn = std::function<bool(T&)>;
+
+    public:
+        /// <summary>
+        /// Event dispatcher takes in and assigns a reference event.
+        /// </summary>
+        EventDispatcher(Event& event) : m_Event(event)
+        {
+        }
+
+
+        template <typename T>
+        /// <summary>
+        /// Event Dispatcher, takes in a handler event from EventFn<T> where T is a class that inherits from Event
+        /// Checks the event type of the handler vs the event if it is the same runs the dispatch and return true
+        /// other it returns false
+        /// </summary>
+        /// @param func EventFn<T> Dispatcher function of a class T where T is a class that inherits from event
+        /// @return Bool, True if this dispatchers event matches the dispatch type false otherwise.
+        bool Dispatch(EventFn<T> func)
+        {
+            if (m_Event.GetEventType() == T::GetStaticType())
+            {
+                m_Event.m_Handled = func(static_cast<T&>(m_Event));
+                return true;
+            }
+            return false;
+        }
+
+    private:
+        Event& m_Event; // Declaration of Event for event dispatcher to use.
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const Event& e)
+    {
+        return os << e.ToString();
+    } // overwrite of the << ostram operator such that this automatically returns its tostring
 }
---
+++ FalseUnion/src/Engine/Headers/Events/KeyEvent.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include <sstream>
+
+#include "Event.h"
+
+namespace FalseUnion
+{
+    // Base Key Event class for others to inherit from, inherits from event.
+    class FALSEUNION_API KeyEvent : public Event
+    {
+    public:
+        /// <summary>
+        /// Returns the key code of this KeyEvent
+        /// </summary>
+        /// @returns int representing key code of this KeyEvent.
+        int GetKeyCode() const { return m_KeyCode; } 
+        EVENT_CLASS_CATEGORY(Keyboard) // Defines the category as keyboard.
+
+    protected:
+        /// <summary>
+        /// Protected constructor for KeyEvent, takes in int for KeyCode.
+        /// </summary>
+        /// @param keycode int to be assigned as this KeyEvents KeyCode
+        KeyEvent(int keycode) : m_KeyCode(keycode)
+        {
+        }
+        
+        int m_KeyCode; // Key code declared as private
+    };
+
+    // key pressed event class inherits from key event
+    class FALSEUNION_API KeyPressedEvent : public KeyEvent
+    {
+    public:
+        /// <summary>
+        /// Constructor for KeyPressedEvent. Takes in key code and repeat count as ints. Assigns both.
+        /// </summary>
+        /// @param keycode int representing the code of the key pressed
+        /// @param repeatCount int representing the number of times the key has repeated.
+        KeyPressedEvent(int keycode, int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount)
+        {
+        } 
+
+        /// <summary>
+        /// Getter for repeat count
+        /// </summary>
+        /// @returns int repeat count of key pressed.
+        int GetRepeatCount() const { return m_RepeatCount; }
+
+        /// <summary>
+        /// Override of ToString for KeyPressedEvent.
+        /// </summary>
+        /// @returns string, String that says "KeyPressedEvent:" then the keycode and its repeat count.
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+            return ss.str();
+        }
+        // macro definition with key pressed
+        EVENT_CLASS_TYPE(KeyPressed) 
+        private:
+        // int for repeat count
+        int m_RepeatCount;
+    };
+}
